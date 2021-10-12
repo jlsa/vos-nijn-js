30524684d54f21ec63be89549f9aa79d75e459dd
jlsa <jlsahoekstra@gmail.com>
2021-10-12 20:03:58 +0200

    Forgot to remove console loggers

--- src/theme-switcher.js
+++ src/theme-switcher.js
@@ -2,19 +2,15 @@ class ThemeSwitcher {
   constructor () {
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
       // dark mode
-      console.log('dark mode')
       document.body.classList.add('dark')
     }
 
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
       // light mode
-      console.log('light mode')
       document.body.classList.add('light')
     }
 
-    console.log(window.matchMedia('(prefers-color-schema: dark)'))
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function (e) {
-      console.log('changed!!', e.matches)
       if (e.matches) {
         document.body.classList.replace('light', 'dark')
       } else {
