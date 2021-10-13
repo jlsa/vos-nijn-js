f4b1a9d02454a302ba42247133acd5b65343ad26
jlsa <jlsahoekstra@gmail.com>
2021-10-13 15:50:41 +0200

    Added Jest and first test

---
+++ tests/shuffle.test.js
@@ -0,0 +1,6 @@
+const { expect } = require('@jest/globals')
+const shuffle = require('../src/helpers/shuffle')
+
+test('Shuffle a sorted list', () => {
+  expect(shuffle([1, 2, 3])).not.toBe([1, 2, 3])
+})
