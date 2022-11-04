9fc3c52abe3c4afe395d97e9c01a453821e30060
Joël Hoekstra <jlsahoekstra@gmail.com>
2022-11-04 11:19:09 +0100

    Add shuffle test

--- tests/shuffle.test.js
+++ tests/shuffle.test.js
@@ -3,4 +3,5 @@ const shuffle = require('../src/helpers/shuffle')
 
 test('Shuffle a sorted list', () => {
   expect(shuffle([1, 2, 3])).not.toBe([1, 2, 3])
+  expect(shuffle([1, 2, 3])).not.toBe([1, 2, 3])
 })
